32c3b468886673c6b46e602697aae44648910b8f
Morteza Kasravi <kasravi@gmail.com>
2023-04-22 22:29:22 +0200

    install

--- scripts.js
+++ scripts.js
@@ -8,6 +8,31 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  showInstallPrompt();
+});
+
+function showInstallPrompt() {
+  const installButton = document.querySelector('#install-button');
+  installButton.style.display = 'block';
+  installButton.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt.');
+      } else {
+        console.log('User dismissed the install prompt.');
+      }
+      deferredPrompt = null;
+    });
+  });
+}
+
+
 // window.onerror = (a, b, c, d, e) => {
 //   //alert(`message: ${a}`+`, source: ${b}`+`, lineno: ${c}`+`, colno: ${d}`+`, error: ${e}`);
 
